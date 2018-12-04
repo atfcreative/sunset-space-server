e5c77a2d236fd9cdde0c9b840476846426f4e471
atfcreative <atfcreative@gmail.com>
2018-12-04 14:57:51 -0800

    add heroku endpoints

--- constants/frontend.js
+++ constants/frontend.js
@@ -1,9 +1,8 @@
 const FRONTEND_DEV_URLS = [ 'http://localhost:3000' ];
 
 const FRONTEND_PROD_URLS = [ 
-    'https://ss-test-client.herokuapp.com',
-    'https://ss-test-server.herokuapp.com/',
-    'mongodb://heroku_pgglj6qc:tqinmqvr9lgresl73c3d1tk5kq@ds119210.mlab.com:19210/heroku_pgglj6qc'
+    'https://sunset-space.herokuapp.com',
+    'https://sunset-space-server.herokuapp.com/',
  ];
 
  module.exports = FRONTEND_PROD_URLS;
